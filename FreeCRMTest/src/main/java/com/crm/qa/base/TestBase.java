5904f40551cc0ada0298b7497a9ba3188753b370
jp <jaihindpatil@gmail.com>
2017-12-31 00:51:16 +0530

    POM framework Login done

---
+++ FreeCRMTest/src/main/java/com/crm/qa/base/TestBase.java
@@ -0,0 +1,64 @@
+package com.crm.qa.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.crm.qa.utils.TestUtil;
+
+public class TestBase {
+
+	public static WebDriver driver;
+	public static Properties prop;
+	
+	public TestBase(){
+		
+		prop = new Properties();
+		try {
+			FileInputStream ip = new FileInputStream("D:\\ClassSelenium\\received\\24July2016 TestNgStarted\\"
+					+ "FreeCRMTest\\src\\main\\java\\com\\crm\\qa\\config\\config.properties");
+			prop.load(ip);
+		    } catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		    } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		   }
+		
+	     } 
+	
+      
+
+    public static void initialization(){
+	String browserName = prop.getProperty("browser");
+	if(browserName.equals("chrome")){
+		System.setProperty("webdriver.chrome.driver", "D:\\ClassSelenium\\driver\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+		driver= new ChromeDriver();
+		//driver.get("http://toolsqa.wpengine.com/automation-practice-form/");
+	  }
+	else if(browserName.equals("FF")){
+		System.setProperty("webdriver.gecko.driver", "D:\\ClassSelenium\\driver\\geckodriver.exe");
+		//System.setProperty("webdriver.gecko.driver", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+		
+		driver= new FirefoxDriver();
+	  }
+	driver.manage().window().maximize();
+	//driver.manage().deleteAllCookies();
+	driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+	driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+	
+
+	driver.get(prop.getProperty("url"));
+	
+	
+	}
+    
+}
