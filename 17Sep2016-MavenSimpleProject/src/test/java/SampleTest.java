dea0f67a5d92c536690c6c4115457f6fad13e57d
jai, patil <jaihindpatil@gmail.com>
2020-02-21 01:10:21 +0530

    Added chromedriver path

--- 17Sep2016-MavenSimpleProject/src/test/java/SampleTest.java
+++ 17Sep2016-MavenSimpleProject/src/test/java/SampleTest.java
@@ -1,6 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,13 +8,15 @@ public class SampleTest {
 
 	@Test
 	public void testLogin(){
-		WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "D:\\ClassSelenium\\seleniumJar\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		driver.get("http://demo.testfire.net");
 		driver.findElement(By.linkText("ONLINE BANKING LOGIN")).click();
 		driver.findElement(By.id("uid")).sendKeys("jsmith");
 		driver.findElement(By.id("passw")).sendKeys("demo1234");
 		driver.findElement(By.id("passw")).submit();
 		
-		Assert.assertEquals(driver.getCurrentUrl(), "http://demo.testfire.net/bank/main.aspx","User login failed");
+		Assert.assertEquals(driver.getCurrentUrl(), "http://demo.testfire.net/bank/main.jsp","User login failed");
+		driver.quit();
 	}
 }
